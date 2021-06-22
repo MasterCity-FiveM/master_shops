2d598ad92980d85addebe80aa9fa674d76631df8
masterking32 <lichwow_masterking@yahoo.com>
2021-06-22 08:11:00 +0430

    ...

--- config.lua
+++ config.lua
@@ -7,10 +7,10 @@ Config.Locale       = 'en'
 
 Config.Map = {
   {name="Tequil-La La",color=27, id=93, x=-565.906, y=276.093, z=100.176},
-  {name="Bahamas",color=27, id=93,x =-1388.409, y =-585.624, z =100.319},
+  --{name="Bahamas",color=27, id=93,x =-1388.409, y =-585.624, z =100.319},
   {name="Strip club",color=27, id=121,x =132.17, y =-1304.42, z =100.319},
   {name="Yellow Jack",color=27, id=93,x =1992.69, y =3058.57, z =100.319},
-  {name="Galaxy",color=27, id=93,x =5.00, y =220.24, z =107.72}, -- Wainting for the nightclub id blip
+  --{name="Galaxy",color=27, id=93,x =5.00, y =220.24, z =107.72}, -- Wainting for the nightclub id blip
   {name="Shop",color=2, id=52,x =26.21539, y =-1347.231, z =29.48206},
   {name="Shop",color=2, id=52,x =373.875, y =325.896, z =100.319},
   {name="Shop",color=2, id=52,x =2557.458, y =382.282, z =100.319},
@@ -32,6 +32,7 @@ Config.Map = {
   {name="Shop",color=2, id=52,x =-1820.523, y =792.518, z =137.118},
   {name="Shop",color=2, id=52,x =1698.388, y =4924.404, z =41.063},
   {name="Digital Shop",color=37, id=521,x =-657.5472, y =-857.0374, z =24.49451},
+  {name="Digital Shop",color=37, id=521,x= -510.39559936523, y=281.68353271484, z= 83.21},
   {name="Gun Shop",color=1, id=150,x =-662.1, y =-935.3, z = 20.8},
   {name="Gun Shop",color=1, id=150,x =810.2, y =-2157.3, z = 28.6},
   {name="Gun Shop",color=1, id=150,x =1693.4, y =3759.5, z = 33.7},
@@ -85,7 +86,7 @@ Config.Zones = {
     },
 
     Bar = {
-        Items = {beer, wine, vodka, tequila, whisky, gintonic, absinthe ,champagne, cigarett, lighter},
+        Items = {beer, vodka, sandis},
         Pos = {
             {x = 127.830,   y = -1284.796, z = 28.280}, --StripClub
             {x = -1393.409, y = -606.624,  z = 29.319}, --Bahamamas
@@ -105,7 +106,8 @@ Config.Zones = {
     DigitalShop = {
         Items = {phone},
         Pos = {
-            {x = -657.5472,   y = -857.0374, z = 23.49451}
+            {x = -657.5472,   y = -857.0374, z = 23.49451},
+            {x= -510.39559936523, y=281.68353271484, z= 82.216064453125},
         }
     },
 	
